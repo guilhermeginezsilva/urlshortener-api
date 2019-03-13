abb9ba99a9072748c307dd1203197c1df9511c6f
Web PDI <webpdi.team@gmail.com>
2019-03-13 04:58:01 -0300

    First Commit

---
+++ src/main/java/br/com/ginezgit/urlshortenerapi/UrlshortenerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ginezgit.urlshortenerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortenerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortenerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ginezgit/urlshortenerapi/UrlshortenerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ginezgit.urlshortenerapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UrlshortenerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
