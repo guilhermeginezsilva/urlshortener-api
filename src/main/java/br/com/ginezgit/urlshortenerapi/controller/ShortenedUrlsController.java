67721ae78608aa85f78a0caf5f744bc5d9a44081
Web PDI <webpdi.team@gmail.com>
2019-03-20 00:23:18 -0300

    Integration with AWS

--- src/main/java/br/com/ginezgit/urlshortenerapi/controller/ShortenedUrlsController.java
+++ src/main/java/br/com/ginezgit/urlshortenerapi/controller/ShortenedUrlsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import br.com.ginezgit.urlshortenerapi.service.UrlsService;
 import br.com.ginezgit.urlshortenerapi.util.validation.ShortenedUrlIdValidator;
 import br.com.ginezgit.urlshortenerapi.util.validation.UrlValidator;
 
+@CrossOrigin
 @RestController
 public class ShortenedUrlsController {
 
